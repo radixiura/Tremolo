3938eb7ccca0911f676d7909c6d9bcc5eb1e181f
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-04-27 09:25:02 +0300

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <cstring>
-
+struct account
+{
+  const int ID;
+  char name[20];
+  char password[20];
+};
+int main()
+{
+  using namespace std;
+  account users[2] = 
+  {
+    {1, "nick", "qwerty"},
+    {2, "sofia", "ytrewq"}
+  };
+cout << "Hello.\n";
+cout << "I am happy because you are using our chat - messenger." << endl;
+cout << "What is your name?" << endl;
+string new_user_name;
+cin >> new_user_name;
+//Блок включения нового логина в базу данных
+cout << "Glad to see you, " << new_user_name << endl; 
+cout << "Now it is time to choose your password\n";
+string new_user_password;
+cin >> new_user_password;
+return 0;
+}
