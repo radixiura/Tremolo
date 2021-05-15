284ca12ded480eca98fd1f2812beb70a546e3a43
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-05-15 14:33:40 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,62 +1,11 @@
-import os
-import sys
-import psycopg2
-from psycopg2 import OperationalError
-
-def create_connection(db_name, db_user, db_password, db_host, db_port):
-    connection = None
-    try:
-        connection = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
-        )
-        print('Connection to PostgreSQL DB sucessful')
-    except OperationalError as e:
-        print(f"The error '{e}' occured")
-    return connection
-
-connection = create_connection("postgres", "postgres", "serv313", "127.0.0.1", "5432")
-
-def create_database(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed successfully")
-    except OperationalError as e:
-        print(f"The error '{e}' occurred")
-
-create_database_query = "CREATE DATABASE sm_app"
-create_database(connection, create_database_query)
-
-connection = create_connection(
-    "sm_app", "postgres", "abc123", "127.0.0.1", "5432"
-)
-
-def execute_query(connection, query):
-    connection.autocommit = True
-    cursor = connection.cursor()
-    try:
-        cursor.execute(query)
-        print("Query executed sucessfully")
-    except OperationalError as e:
-        print(f"The error '{e}' has occured")
-
-create_users_table = """
-CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    login TEXT NOT NULL,
-    password TEXT NOT NULL,
-    age INTEGER,
-    info TEXT
-)
-"""
-
-execute_query(connection, create_users_table)
-
+def menu():
+    print('Выбери, что бы ты хотел сделать?')
+    print('Отправить сообщение - 1')
+    print('Узнать курс BTC - 2')
+    print('Узнать свой ip - адрес - 3')
+    print('Узнать свои настройки сети - 4')
+    print('Завершить работу - 5')
+    print('Связаться с разработчиком - 0')
 
 def privetstvie():
     print("Привет.")
@@ -70,28 +19,15 @@ if answer1 == 'Да':
     #   pass
     # else:
     #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
-
     print('Ваш логин - ' + old_user_login)
     old_user_password = input("Введите ваш пароль: ")
-
     # if old_user_password exist in DB:
     #   pass
     # else:
     #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
-
-    def menu():
-        print('Выбери, что бы ты хотел сделать?')
-        print('Отправить сообщение - 1')
-        print('Узнать курс BTC - 2')
-        print('Узнать свой ip - адрес - 3')
-        print('Узнать свои настройки сети - 4')
-        print('Завершить работу - 5')
-        print('Связаться с разработчиком - 0')
-    menu()
-    
 else:
     print('Думаю, тебе стоит зарегистрироваться.')
-    new_user_login = input('Введи свой новый логин')
+    new_user_login = input('Введи свой новый логин: ')
     # while new_user_login exists in BD
     # print('Этот логин уже занят')
     # print('Попробуйте ввести другой')
@@ -99,7 +35,7 @@ else:
     # if new_user_login not exists in BD:
     # break
     print('Ваш новый логин - ', new_user_login)
-    print('Самое время придумать пароль!')
+    print('Самое время придумать пароль! Рекомендуем использовать пароль, состоящий более чем из 8ми символов.')
     new_user_password = input('Введи свой новый пароль')
     print('Подтвердите ваш пароль')
     new_user_password_confirmation = input()
@@ -108,12 +44,10 @@ else:
         new_user_password_confirmation = input()
         if new_user_password_confirmation == new_user_password:
             break
-            continue
     print('Отлично, ты зарегистрирован.')
 
-
-    menu()
-
+menu()
+user_choice = input()
 if user_choice == 1:
     print('Введи логин своего знакомого')
     new_request = input()
