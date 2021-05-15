df7c1cfb8e5895c79044a3abc914d4fe49e812e1
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-05-15 22:05:15 +0300

    Update main.cpp

--- cpp_module/main.cpp
+++ cpp_module/main.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
-#include <cstring>
+#include <cstdio>
 
+struct User
+{
+  char login[256];
+  char password[256];
+  char keyword[256];
+};
+
+int greeting()
+{
+  printf("Привет.");
+  printf("Рад видеть тебя.");
+  return 0;
+}
+
+int main()
+{
+  greeting();
+  
+  return 0;
+}
