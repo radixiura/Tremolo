c42aa86daec2c5800fe4c8b5b2a5c0e4911ebd63
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-05-15 22:00:22 +0300

    Update main.cpp

--- cpp_module/main.cpp
+++ cpp_module/main.cpp
@@ -1,23 +1,3 @@
 #include <iostream>
 #include <cstring>
-struct user
-{
-  const int ID;
-  char name[20];
-  char password[20];
-};
-int main()
-{
-using namespace std;
-cout << "Hello.\n";
-cout << "I am happy because you are using our chat - messenger." << endl;
-cout << "What is your name?" << endl;
-string new_user_name;
-cin >> new_user_name;
-cout << "Glad to see you, " << new_user_name << endl;
-cout << "Now it is time to set your password\n";
-//Блок создания нового пользователя
-string new_user_password;
-cin >> new_user_password;
-return 0;
-}
+
