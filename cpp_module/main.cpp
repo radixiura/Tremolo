9517fefc9a7d563efe16188b85f8904ea1527196
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-05-19 15:59:52 +0300

    Update main.cpp

--- cpp_module/main.cpp
+++ cpp_module/main.cpp
@@ -47,6 +47,7 @@ int new_user_registration()
 
 int main_menu()
 {
+  using namespace std;
   printf("Меню:\n");
   printf("Отправить сообщение - 1\n");
   printf("Узнать курс BTC - 2\n");
@@ -55,11 +56,79 @@ int main_menu()
   printf("Узнать больше о программе - 5\n");
   printf("Завершить работу - 6\n");
   printf("Связаться с разработчиком - 0\n");
+  printf("Введите 1, 2, 3, 4, 5, 6 или 0: ");
+  return 0;
+}
+
+int case5()
+{
+  using namespace std;
+  printf("Что бы вы хотели узнать?\n");
+  printf("Язык и технологии, используемые при создании Tremolo - 1\n");
+  printf("Гарантии безопасности при работе с Tremolo - 2\n");
+  printf("Цели и задачи Tremolo - 3\n");
+  printf("Назад - 0\n");
+  printf("Введите 1, 2, 3 или 0: ");
+  int user_choice5;
+  cin >> user_choice5;
+  switch(user_choice5)
+  {
+    case 1:
+      printf("В данный момент есть два клиента Tremolo. Один полностью написан на C++, второй на Python3.\n");
+      break;
+    case 2:
+      printf("Вы можете просто отсосать мой член, если не верите в безопасность нашего чата.\n");
+      break;
+    case 3:
+      printf("Основной задачей Tremolo является скорейшая продажа этой хуйни и последующий гешефт.\n");
+      break;
+    case 0:
+      printf("НАЗАД БЛЯДЬ.\n");
+      break;
+  }
+  return 0;
+}
+
+int templatemo()
+{
+  using namespace std;
+  int user_choice_from_main_menu;
+  cin >> user_choice_from_main_menu;
+  switch(user_choice_from_main_menu)
+  {
+    case 1:
+      printf("lalala");
+      //функция
+    case 2:
+      printf("Отчет по курсу BTC к валютам.\n");
+      //api
+      break;
+    case 3:
+      printf("Ваш текущий ip-адрес");
+      //api
+      break;
+    case 4:
+      printf("Что бы вы хотели настроить?");
+    case 5:
+      case5();
+    case 6:
+      printf("До свидания!");
+      //Выход
+      break;
+  }
+  return 0;
+}
+
+int user_answer_from_main_menu()
+{
+  using namespace std;
+  templatemo();
   return 0;
 }
 
 int main()
 {
+  setlocale(LC_ALL, "Russian");
   using namespace std;
   greeting();
   cout << "Ты уже имеешь аккаунт? Введи 1 если да, 0 если нет: ";
@@ -80,6 +149,6 @@ int main()
     break;
   }
   main_menu();
-
+  user_answer_from_main_menu();
   return 0;
 }
