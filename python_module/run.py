74a62ae3b34e8fa824347d13b2b2c97aa21938ae
Radix_iura <69742759+radixiura@users.noreply.github.com>
2021-05-28 01:31:32 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -8,45 +8,10 @@ functions.greeting()
 user_answer = input('Ты уже имеешь аккаунт?: ')
 
 if user_answer == 'Да':
-    print('Отлично.')
-    old_user_login = input("Введите ваш логин: ")
-    # if old_user_login exist in DB:
-    #   pass
-    # else:
-    #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
-    print('Ваш логин: ' + old_user_login)
-    old_user_password = input("Введите ваш пароль: ")
-    # if old_user_password exist in DB:
-    #   pass
-    # else:
-    #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
-else:
-    print("Думаю, вам стоит зарегистрироваться. Ваш новый логин должен содержать более 8 символов")
-    new_user_login = input("Введите ваш новый логин: ")
-    # while new_user_login exists in BD
-    # print('Этот логин уже занят')
-    # print('Попробуйте ввести другой')
-    # new_user_login = input()
-    # if new_user_login not exists in BD:
-    # break
-    print('Ваш новый логин - ', new_user_login)
-    print('Самое время придумать пароль! Он должен содержать более 8 символов.')
-    new_user_password = input("Введите ваш новый пароль: ")
-    while len(new_user_password) <= 8:
-        print('Пароль должен содержать более 8ми символов.')
-        new_user_password = input()
-        if len(new_user_password) > 8:
-            break
-    number_of_characters = len(new_user_password)
-    print('Подтвердите ваш пароль')
-    new_user_password_confirmation = input()
-    while new_user_password != new_user_password_confirmation:
-        print('Введенные пароли не совпадают. Попробуйте еще раз ввести пароль')
-        new_user_password_confirmation = input()
-        if new_user_password_confirmation == new_user_password:
-            break
-    print("Количество символов в вашем новом пароле: ", number_of_characters)
-    print('Отлично, вы зарегистрированы.')
+    functions.old_user_registration()
+elif user_answer == "Нет":
+    functions.new_user_registration()
+
 
 functions.main_menu()
 
