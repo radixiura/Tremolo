90666413488cc39a10b43d6c671d9aaa58c82ffc
Radix_iura <radix_iura@protonmail.com>
2021-10-05 11:58:40 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# Connecting a file with functions
 from modules import functions
-# from modules import postgresql_connection
 
+# from modules import postgresql_connection
 # postgresql_connection.connect_postgresql()
 
-functions.greeting()
+print("Привет!")
 
-user_answer = input('Вы уже имеете аккаунт?: ')
+# Choosing between login and registration
 
-if user_answer == "Да" or "да":
+user_answer = input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ')
+if user_answer.lower() == "1":
     functions.old_user_registration()
-elif user_answer == "Нет" or "нет":
+if user_answer.lower() == "2":
     functions.new_user_registration()
+else:
+    user_second_answer = input('''Мы не совсем поняли ваш ответ. 
+    Введите 1 если вы уже имеете аккаунт; 0 - если хотите зарегистрироваться.''')
+    while user_second_answer != "1" or "0":
+        user_second_answer = input('''Мы не совсем поняли ваш ответ.
+        Введите 1 если вы уже имеете аккаунт; 0 - если хотите зарегистрироваться.''')
+        if user_answer == 1 or 2:
+            break
