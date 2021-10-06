ef66b47eff6342bfe5b8906d8f3ffd5140f1dbcd
Radix_iura <radix_iura@protonmail.com>
2021-10-06 16:38:40 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -10,16 +10,17 @@ print("Привет!")
 
 # Choosing between login and registration
 
-user_answer = input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ')
-if user_answer.lower() == "1":
-    functions.old_user_registration()
-if user_answer.lower() == "2":
-    functions.new_user_registration()
-else:
-    user_second_answer = input('''Мы не совсем поняли ваш ответ. 
-    Введите 1 если вы уже имеете аккаунт; 0 - если хотите зарегистрироваться.''')
-    while user_second_answer != "1" or "0":
-        user_second_answer = input('''Мы не совсем поняли ваш ответ.
-        Введите 1 если вы уже имеете аккаунт; 0 - если хотите зарегистрироваться.''')
-        if user_answer == 1 or 2:
-            break
+checking = True
+
+while checking:
+    user_answer = int(input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: '))
+
+    if user_answer == 1:
+        functions.old_user_authentication()
+        functions.main_menu()
+    elif user_answer == 0:
+        functions.new_user_registration()
+        functions.main_menu()
+    else:
+        print("Вы ввели что - то странное!")
+        user_answer = input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ')
