0fb77065f75d539cfd8849f278a90198375d901e
Radix_iura <radix_iura@protonmail.com>
2021-10-13 11:41:50 +0300

    Update functions.h

--- cpp_module/bin/functions.h
+++ cpp_module/bin/functions.h
@@ -1,62 +1,13 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+                                                                                                                                       
 
 using namespace std;
-
-
-//  Entry functions
-
-
-int new_user_registration()
-{
-	printf("Самое время зарегистрироваться. Введите свой новый логин: ");
-	char new_user_login; cin >> new_user_login;
-	cout << "Ваш новый логин: " << new_user_login << endl;
-	printf("Самое время придумать пароль. Он должен содержать более 8ми символов: ");
-	char new_user_password; cin >> new_user_password;
-	printf("Введите свой новый пароль еще раз: ");
-	char new_user_password_confirmation; cin >> new_user_password_confirmation;
-	while (new_user_password != new_user_password_confirmation)
-  	{
-    	printf("Введенные пароли не сходятся. Попробуйте еще раз.\n");
-    	cout << "Введите пароль: "; cin >> new_user_password_confirmation;
-  	}
-  	return 0; 
-}
-
-
-int old_user_authentication()
-{
-	using namespace std;
-	printf("Введите свой логин: ");
-	char old_user_login; cin >> old_user_login;
-	cout << "Ваш логин: " << old_user_login << endl;
-	printf("Введите свой пароль: ");
-	char old_user_password; cin >> old_user_password;
-	return 0;
-}
 
 
 //  Menu functions
 
 
-int main_menu()
-{
-	using namespace std;
-  	printf("Меню:\n");
-  	printf("Отправить сообщение - 1\n");
-  	printf("Узнать курс BTC - 2\n");
-  	printf("Узнать свой ip - адрес - 3\n");
-  	printf("Настройки аккаунта - 4\n");
-  	printf("Узнать больше о программе - 5\n");
-  	printf("Завершить работу - 6\n");
-  	printf("Связаться с разработчиком - 0\n");
-  	printf("Введите 1, 2, 3, 4, 5, 6 или 0: ");
-  	return 0;
-}
-
-	
 int main_menu_modules_1()
 {
 	printf("Введите логин, на который нужно отправить сообщение: ");
@@ -150,16 +101,106 @@ int main_menu_modules_5()
 }
 
 
-int main_menu_modules6()
+int main_menu_modules_6()
 {
 	//std::cout << "До свидания, " << User.chel.user_login << "!";
 	exit(1);
 }
 
 
-int main_menu_modules0()
+int main_menu_modules_0()
 {
 	printf("В настоящий момент я хикканю и у меня нет желания говорить.\n");
+	return 0;
+}
+
+
+int main_menu()
+{
+	using namespace std;
+  	printf("Меню:\n");
+  	printf("Отправить сообщение - 1\n");
+  	printf("Узнать курс BTC - 2\n");
+  	printf("Узнать свой ip - адрес - 3\n");
+  	printf("Настройки аккаунта - 4\n");
+  	printf("Узнать больше о программе - 5\n");
+  	printf("Завершить работу - 6\n");
+  	printf("Связаться с разработчиком - 0\n");
+  	printf("Введите 1, 2, 3, 4, 5, 6 или 0: ");
+  	int user_choice; cin >> user_choice;
+  	switch (user_choice)
+  	{
+  		case 1:
+  			main_menu_modules_1();
+  			break;
+  		case 2:
+  			main_menu_modules_2();
+  			break;
+  		case 3:
+  			main_menu_modules_3();
+  			break;
+  		case 4:
+  			main_menu_modules_4();
+  			break;
+  		case 5:
+  			main_menu_modules_5();
+  			break;
+  		case 6:
+  			main_menu_modules_6();
+  			break;
+  		case 0:
+  			main_menu_modules_0();
+  			break;
+  	}
+  	main_menu();
+  	return 0;
+}
+
+
+int new_user_registration()
+{
+	printf("Самое время зарегистрироваться. Введите свой новый логин: ");
+	char new_user_login; cin >> new_user_login;
+	cout << "Ваш новый логин: " << new_user_login << endl;
+	printf("Самое время придумать пароль. Он должен содержать более 8ми символов: ");
+	char new_user_password; cin >> new_user_password;
+	printf("Введите свой новый пароль еще раз: ");
+	char new_user_password_confirmation; cin >> new_user_password_confirmation;
+	while (new_user_password != new_user_password_confirmation)
+  	{
+    	printf("Введенные пароли не сходятся. Попробуйте еще раз.\n");
+    	cout << "Введите пароль: "; cin >> new_user_password_confirmation;
+  	}
+  	return 0; 
+}
+
+
+char get_old_login()
+{
+	printf("Введите свой логин: ");
+	char login;
+	cin >> login;
+	return login;
+}
+
+
+char get_old_password()
+{
+	printf("Введите свой пароль: ");
+	char password;
+	cin >> password;
+	return password;
+}
+
+
+int old_user_authentication()
+{
+	using namespace std;
+	printf("Введите свой логин: ");
+	char old_user_login; cin >> old_user_login;
+	cout << "Ваш логин: " << old_user_login << endl;
+	printf("Введите свой пароль: ");
+	char old_user_password; cin >> old_user_password;
 	return 0;
 }
 
