eac865621307bdf9d65d8234569ebe86a5398211
Radix_iura <radix_iura@protonmail.com>
2021-10-17 21:34:04 +0300

    Update main.cpp

--- cpp_module/bin/main.cpp
+++ cpp_module/bin/main.cpp
@@ -12,39 +12,11 @@
 //#include "already_existing_user.hpp"
 
 
-int get_user_answer()
-{
-	while (true)
-	{
-		printf("Привет!\n");
-		printf("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ");
-		int user_answer; cin >> user_answer;
-		if (user_answer == 1 || user_answer == 0)
-			return user_answer;
-		else
-			printf("siegout");
-	}
-}
-
-
-void log_in_choice(int get_user_answer)
-{
-	if (get_user_answer == '1')
-		old_user_authentication();
-	if (get_user_answer == '0')
-		new_user_registration();
-}
-
-
 int main()
 {
   using namespace std;
   setlocale(LC_ALL, "Russian");
   int user_answer = get_user_answer();
-  while (get_user_answer() != 1 && 0)
-  {
-  	printf("Высад");
-  }
   switch(user_answer)
   {
   	case 0:
