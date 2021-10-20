b9294a1780caf4e3640be714522f55befdd66d7d
Radix_iura <radix_iura@protonmail.com>
2021-10-20 17:09:08 +0300

    Update functions.h

--- cpp_module/bin/functions.h
+++ cpp_module/bin/functions.h
@@ -1,217 +1,167 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-                                                                                                                                       
-
-using namespace std;
-
-
-//  Menu functions
+import sys
+import requests
+import bs4
+from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QTextEdit, QGridLayout, QApplication)
 
 
-int main_menu_modules_1()
-{
-	printf("Введите логин, на который нужно отправить сообщение: ");
-	string login_to_message; getline(cin, login_to_message);
-	// Проверка существования логина
-	cout << "Введите сообщения для " << login_to_message << ":" << endl;
-	string message; getline(cin, message);
-	cout << message;
-	// Отправка сообщения
-	return 0;
-}
+#  Entry functions
 
 
-int main_menu_modules_2()
-{
-	printf("Курс BTC:");
-	//api
-	return 0;
-}
+def old_user_authentication():
+    old_user_login = input("Введите ваш логин: ")
+    print(f"Ваш логин: {old_user_login}")
+    # if old_user_login exist in DB:
+    #   pass
+    # else:
+    #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
+    old_user_password = input("Введите ваш пароль: ")
+    if len(old_user_password) > 8:
+        print(f"{old_user_login}, вы успешно вошли.")
+    # if old_user_password exist in DB:
+    #   pass
+    # else:
+    #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
 
 
-int main_menu_modules_3()
-{
-	printf("Текущий IP - адрес:");
-	//api
-	return 0;
-}
+def new_user_registration():
+    print("Самое время придумать логин.")
+    print("Он должен содержать более 8ми символов.")
+    new_user_login = input("Введите ваш новый логин: ")
+    while len(new_user_login) < 8:
+        print('Логин должен содержать более 8ми символов. Попробуйте заново.')
+        new_user_login = input()
+        if len(new_user_login) > 8:
+            break
+    # while new_user_login exists in BD
+    # print('Этот логин уже занят')
+    # print('Попробуйте ввести другой')
+    # new_user_login = input()
+    # if new_user_login not exists in BD:
+    # break
+    print(f"Ваш новый логин: {new_user_login}")
+    print("Самое время придумать пароль. Он должен содержать более 8ми символов.")
+    new_user_password = input("Введите ваш новый пароль: ")
+    while len(new_user_password) < 8:
+        print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
+        new_user_password = input()
+        if len(new_user_password) > 8:
+            break
+    new_user_password_confirmation = input("Подтвердите ваш пароль: ")
+    while new_user_password != new_user_password_confirmation:
+        print('Введенные пароли не совпадают. Попробуйте еще раз ввести пароль')
+        new_user_password_confirmation = input()
+        if new_user_password_confirmation == new_user_password:
+            break
+
+#  Menu functions
 
 
-int main_menu_modules_4()
-{
-	printf("Что бы вы хотели настроить?\n");
-	printf("Изменить логин - 1\n");
-	printf("Изменить пароль - 2\n");
-	printf("Изменить аватар - 3\n");
-	printf("Удалить аккаунт - 4\n");
-	printf("Назад - 0\n");
-	printf("Введите 1, 2, 3, 4 или 0: ");
-	int user_choice4; cin >> user_choice4;
-	string password_confirmation;
-	switch(user_choice4)
-	{
-		case 1:
-			printf("Для продолжения введите ваш пароль: ");
-			cin >> password_confirmation;
-			//
-			printf("Логин успешно изменен!");
-			break;
-		case 2:
-			printf("Для продолжения введите ваш пароль: ");
-			cin >> password_confirmation;
-			//
-			printf("Пароль успешно изменен!");
-			break;
-		case 3:
-			printf("Вставьте ссылку на ваш новый аватар");
-			break;
-		case 4:
-			printf("Для продолжения введите ваш пароль: ");
-			cin >> password_confirmation;
-			//
-			printf("Ваш аккаунт удален. До свидания!");
-      		exit(1);
-      		break;
-	}
-	return 0;
-}
+def main_menu():
+    print("Меню:")
+    print("Отправить сообщение - 1")
+    print("Узнать курс BTC - 2")  # ready
+    print("Узнать свой ip - адрес - 3")  # ready
+    print("Настройки аккаунта - 4")
+    print("Узнать больше о программе - 5")  # ready
+    print("Завершить работу - 6")  # ready
+    print("Связаться с разработчиком - 0")
+    user_choice = input("Выберите действие: ")
+    if user_choice == "1":
+        menu1()
+    elif user_choice == "2":
+        menu2()
+    elif user_choice == "3":
+        menu3()
+    elif user_choice == "4":
+        menu4()
+    elif user_choice == "5":
+        menu5()
+    elif user_choice == "6":
+        menu6()
+    elif user_choice == "0":
+        print('Значит, ты выбрал смерть...')
 
 
-int main_menu_modules_5()
-{ 
-	printf("Что бы вы хотели узнать?\n");
-	printf("Язык и технологии, используемые при создании Tremolo - 1\n");
-	printf("Гарантии безопасности при работе с Tremolo - 2\n");
-	printf("Цели и задачи Tremolo - 3\n");
-	printf("Назад - 0\n");
-	printf("Введите 1, 2, 3 или 0: ");
-	int user_choice5; cin >> user_choice5;
-	switch(user_choice5)
-	{
-	case 1:
-      printf("В данный момент есть два клиента Tremolo. Один полностью написан на C++, второй на Python3. Используемая СУДБ - PostgreSQL. Также, используется некоторая скриптовая поддержка Shell. Графический интерфейс программы построен на библиотеке Qt.\n");
-      break;
-	case 2:
-      printf("Вы можете просто связаться с разработчиком, если не верите в безопасность нашего чата.\n");
-      break;
-	case 3:
-      printf("Специально для урегулирования вражды между компилируемыми и интерпретируемыми языками программирования программа для нашего чата написана на c ++ и python.\n");
-      break;
-	case 0:
-      printf("НАЗАД\n");
-      break;
-	}
-	return 0;
-}
+def menu1():
+    login_for_new_message = input("Введите логин, на который нужно отправить сообщение: ")
+    message = input(f"Введите сообщение для {login_for_new_message}: ")
+    print(message)
+    # if login_for_new_message exists in DB
+    #   new_message = input("Введите текст вашего сообщения: ")
+    # elif login_for_new_message not exists in DB
+    #   print("Введенного логина не существует в нашей базе данных!)
+    # else:
+    #   print("Произошла ошибка")
+    # print(f"Введите сообщение для {login_for_message} : ")
 
 
-int main_menu_modules_6()	// Ready
-{
-	//std::cout << "До свидания, " << User.chel.user_login << "!";
-	exit(1);
-}
+def menu2():
+    response = requests.get('https://apirone.com/api/v2/ticker?currency=btc')
+    if response:
+        print('Отчет по курсу BTC к валютам готов.')
+    else:
+        print('Произошла ошибка.')
+    print(response.text)
+    main_menu()
 
 
-int main_menu_modules_0()
-{
-	printf("В настоящий момент я хикканю и у меня нет желания говорить.\n");
-	return 0;
-}
+def menu3():
+    print("Ваш текущий ip-адрес")
+    pepsi = requests.get('https://2ip.ua/ru/')
+    sprite = bs4.BeautifulSoup(pepsi.text, "html.parser")
+    ip_address_info = sprite.select(" .ipblockgradient .ip")[0].getText()
+    print(ip_address_info)
+    main_menu()
 
 
-int main_menu()
-{
-  	printf("Меню:\n");
-  	printf("Отправить сообщение - 1\n");
-  	printf("Узнать курс BTC - 2\n");
-  	printf("Узнать свой ip - адрес - 3\n");
-  	printf("Настройки аккаунта - 4\n");
-  	printf("Узнать больше о программе - 5\n");
-  	printf("Завершить работу - 6\n");
-  	printf("Связаться с разработчиком - 0\n");
-  	printf("Введите 1, 2, 3, 4, 5, 6 или 0: ");
-  	int user_choice; cin >> user_choice;
-  	switch (user_choice)
-  	{
-  		case 1:
-  			main_menu_modules_1();
-  			break;
-  		case 2:
-  			main_menu_modules_2();
-  			break;
-  		case 3:
-  			main_menu_modules_3();
-  			break;
-  		case 4:
-  			main_menu_modules_4();
-  			break;
-  		case 5:
-  			main_menu_modules_5();
-  			break;
-  		case 6:
-  			main_menu_modules_6();
-  			break;
-  		case 0:
-  			main_menu_modules_0();
-  			break;
-  	}
-  	main_menu();
-  	return 0;
-}
+def menu4():
+    print("Что бы вы хотели настроить?")
+    print("Изменить логин - 1")
+    print("Изменить пароль - 2")
+    print("Изменить аватар - 3")
+    print("Удалить аккаунт - 4")
+    print("Назад - 0")
+    user_choice4 = input("Выберите действие: ")
+    if user_choice4 == "1":
+        password_confirmation = input("Для продолжения введите ваш пароль: ")
+        global password
+        while password_confirmation != password:
+            print("Не слишком похоже на ваш пароль. Попробуйте заново.")
+            password_confirmation = input()
+            if password_confirmation == password:
+                login = input("Введите ваш новый логин:")
+                print(f"Ваш новый логин - {login} !")
+                break
+    elif user_choice4 == "2":
+        password_confirmation = input("Для продолжения введите ваш пароль: ")
+    elif user_choice4 == "3":
+        print("Вставьте ссылку на ваш новый аватар: ")
+    elif user_choice4 == "4":
+        password_confirmation = input("Для продолжения введите ваш пароль: ")
+        print("Ваш аккаунт удален. До свидания!")
+        exit()
+        main_menu()
 
 
-int new_user_registration()
-{
-	printf("Самое время зарегистрироваться. Введите свой новый логин: ");
-	string new_user_login; getline(cin, new_user_login);
-	cout << "Ваш новый логин: " << new_user_login << endl;
-	printf("Самое время придумать пароль. Он должен содержать более 8ми символов: ");
-	string new_user_password; getline(cin, new_user_password);
-	printf("Введите свой новый пароль еще раз: ");
-	string new_user_password_confirmation; getline(cin, new_user_password_confirmation);
-	while (new_user_password != new_user_password_confirmation)
-  	{
-    	printf("Введенные пароли не сходятся. Попробуйте еще раз.\n");
-    	cout << "Введите пароль: "; cin >> new_user_password_confirmation;
-  	}
-  	return 0; 
-}
+def menu5():
+    print("Что бы вы хотели узнать?")
+    print('Язык и технологии, используемые при создании Tremolo - 1')
+    print('Гарантии безопасности при работе с Tremolo - 2')
+    print('Цели и задачи Tremolo - 3')
+    print('Назад - 0')
+    user_choice5 = input("Выберите действие: ")
+    if user_choice5 == "1":
+        print("В данный момент есть два клиента Tremolo. Один полностью написан на C++, второй на Python3.")
+    elif user_choice5 == "2":
+        print("Вы можете просто не ныть, если не верите в безопасность нашего чата.")
+    elif user_choice5 == "3":
+        print("Основной задачей Tremolo является скорейший гешефт.")
+        main_menu()
+    elif user_choice5 == "0":
+        print("НАЗАД")
+    main_menu()
 
 
-char get_old_login()
-{
-	printf("Введите свой логин: ");
-	char login;
-	cin >> login;
-	return login;
-}
-
-
-char get_old_password()
-{
-	printf("Введите свой пароль: ");
-	char password;
-	cin >> password;
-	return password;
-}
-
-
-int old_user_authentication()
-{
-	printf("Введите свой логин: ");
-	string old_user_login; getline(cin, old_user_login);
-	cout << "Ваш логин: " << old_user_login << endl;
-	printf("Введите свой пароль: ");
-	string old_user_password; getline(cin, old_user_password);
-	return 0;
-}
-
-int get_user_answer()
-{
-	printf("Привет!\n");
-	printf("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ");
-	int user_answer; cin >> user_answer; cin.ignore(32767, '\n');
-	return user_answer;
-}
-
-#endif
+def menu6():
+    print('До свидания!')
+    exit()
