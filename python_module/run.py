06cc0888165254840ccc26d460a16e425154cf5a
Radix_iura <radix_iura@protonmail.com>
2021-10-20 18:04:38 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -5,22 +5,15 @@ from modules import functions
 
 # from modules import postgresql_connection
 # postgresql_connection.connect_postgresql()
-
 print("Привет!")
-
-# Choosing between login and registration
-
+user_answer = input("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ")
 checking = True
-
 while checking:
-    user_answer = int(input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: '))
-
-    if user_answer == 1:
+    if user_answer == "0":
         functions.new_user_registration()
         functions.main_menu()
-    elif user_answer == 0:
+    elif user_answer == "1":
         functions.old_user_authentication()
         functions.main_menu()
     else:
-        print("Вы ввели что - то странное!")
-        user_answer = input('Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ')
+        print("Ошибка. Вы ввели что - то странное.")
