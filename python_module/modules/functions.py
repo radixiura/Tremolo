c8457373f4b1964632abd93da7e23512d16aace7
Radix_iura <radix_iura@protonmail.com>
2021-10-20 18:04:21 +0300

    Update functions.py

--- python_module/modules/functions.py
+++ python_module/modules/functions.py
@@ -3,94 +3,7 @@ import requests
 import bs4
 
 
-#  Entry functions
-
-
-def new_user_registration():
-    print("Самое время придумать логин.")
-    print("Он должен содержать более 8ми символов.")
-    new_user_login = input("Введите ваш новый логин: ")
-    while len(new_user_login) < 8:
-        print('Логин должен содержать более 8ми символов. Попробуйте заново.')
-        new_user_login = input()
-        if len(new_user_login) > 8:
-            break
-    # while new_user_login exists in BD
-    # print('Этот логин уже занят')
-    # print('Попробуйте ввести другой')
-    # new_user_login = input()
-    # if new_user_login not exists in BD:
-    # break
-    print(f"Ваш новый логин: {new_user_login}")
-    print("Самое время придумать пароль. Он должен содержать более 8ми символов.")
-    new_user_password = input("Введите ваш новый пароль: ")
-    while len(new_user_password) < 8:
-        print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
-        new_user_password = input()
-        if len(new_user_password) > 8:
-            break
-    new_user_password_confirmation = input("Подтвердите ваш пароль: ")
-    while new_user_password != new_user_password_confirmation:
-        print('Введенные пароли не совпадают. Попробуйте еще раз ввести пароль')
-        new_user_password_confirmation = input()
-        if new_user_password_confirmation == new_user_password:
-            break
-    return new_user_login
-
-
-def get_old_login():
-    old_login = input("Введите свой логин: ")
-    return old_login
-
-
-def get_old_password():
-    old_password = input("Введите свой пароль: ")
-    return old_password
-
-
-def old_user_authentication():
-    old_user_login = input("Введите ваш логин: ")
-    print(f"Ваш логин: {old_user_login}")
-    # if old_user_login exist in DB:
-    #   pass
-    # else:
-    #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
-    old_user_password = input("Введите ваш пароль: ")
-    if len(old_user_password) > 8:
-        print(f"{old_user_login}, вы успешно вошли.")
-    # if old_user_password exist in DB:
-    #   pass
-    # else:
-    #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
-
-
 #  Menu functions
-
-
-def main_menu():
-    print("Меню:")
-    print("Отправить сообщение - 1")
-    print("Узнать курс BTC - 2")  # ready
-    print("Узнать свой ip - адрес - 3")  # ready
-    print("Настройки аккаунта - 4")
-    print("Узнать больше о программе - 5")  # ready
-    print("Завершить работу - 6")  # ready
-    print("Связаться с разработчиком - 0")
-    user_choice = input("Выберите действие: ")
-    if user_choice == "1":
-        main_menu_modules_1()
-    elif user_choice == "2":
-        main_menu_modules2()
-    elif user_choice == "3":
-        main_menu_modules3()
-    elif user_choice == "4":
-        main_menu_modules4()
-    elif user_choice == "5":
-        main_menu_modules5()
-    elif user_choice == "6":
-        main_menu_modules6()
-    elif user_choice == "0":
-        main_menu_modules0()
 
 
 def main_menu_modules_1():
@@ -179,9 +92,91 @@ def main_menu_modules6():
 
 
 def main_menu_modules0():
-    login1 = old_user_authentication()
-    login2 = new_user_registration()
-    if login1 > login2:
-        print("Обсада")
-    else:
-        print("Всада")
+    print("В настоящий момент я хикканю и у меня нет желания говорить.")
+
+
+def new_user_registration():
+    print("Самое время придумать логин.")
+    print("Он должен содержать более 8ми символов.")
+    new_user_login = input("Введите ваш новый логин: ")
+    while len(new_user_login) < 8:
+        print('Логин должен содержать более 8ми символов. Попробуйте заново.')
+        new_user_login = input()
+        if len(new_user_login) > 8:
+            break
+    # while new_user_login exists in BD
+    # print('Этот логин уже занят')
+    # print('Попробуйте ввести другой')
+    # new_user_login = input()
+    # if new_user_login not exists in BD:
+    # break
+    print(f"Ваш новый логин: {new_user_login}")
+    print("Самое время придумать пароль. Он должен содержать более 8ми символов.")
+    new_user_password = input("Введите ваш новый пароль: ")
+    while len(new_user_password) < 8:
+        print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
+        new_user_password = input()
+        if len(new_user_password) > 8:
+            break
+    new_user_password_confirmation = input("Подтвердите ваш пароль: ")
+    while new_user_password != new_user_password_confirmation:
+        print('Введенные пароли не совпадают. Попробуйте еще раз ввести пароль')
+        new_user_password_confirmation = input()
+        if new_user_password_confirmation == new_user_password:
+            break
+    return new_user_login
+
+
+def get_old_login():
+    old_login = input("Введите свой логин: ")
+    return old_login
+
+
+def get_old_password():
+    old_password = input("Введите свой пароль: ")
+    return old_password
+
+
+def old_user_authentication():
+    old_user_login = input("Введите ваш логин: ")
+    print(f"Ваш логин: {old_user_login}")
+    # if old_user_login exist in DB:
+    #   pass
+    # else:
+    #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
+    old_user_password = input("Введите ваш пароль: ")
+    if len(old_user_password) > 8:
+        print(f"{old_user_login}, вы успешно вошли.")
+    # if old_user_password exist in DB:
+    #   pass
+    # else:
+    #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
+
+
+#  Menu functions
+
+
+def main_menu():
+    print("Меню:")
+    print("Отправить сообщение - 1")
+    print("Узнать курс BTC - 2")  # ready
+    print("Узнать свой ip - адрес - 3")  # ready
+    print("Настройки аккаунта - 4")
+    print("Узнать больше о программе - 5")  # ready
+    print("Завершить работу - 6")  # ready
+    print("Связаться с разработчиком - 0")
+    user_choice = input("Выберите действие: ")
+    if user_choice == "1":
+        main_menu_modules_1()
+    elif user_choice == "2":
+        main_menu_modules2()
+    elif user_choice == "3":
+        main_menu_modules3()
+    elif user_choice == "4":
+        main_menu_modules4()
+    elif user_choice == "5":
+        main_menu_modules5()
+    elif user_choice == "6":
+        main_menu_modules6()
+    elif user_choice == "0":
+        main_menu_modules0()
