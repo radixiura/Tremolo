73c34708375833d9cbc407dd0eb46b426b689275
Radix_iura <radix_iura@protonmail.com>
2021-10-23 00:14:21 +0300

    Delete already_existing_user.hpp

--- cpp_module/bin/already_existing_user.hpp
+++
@@ -1,7 +0,0 @@
-class Already_existing_user
-{
-  public:
-    const std::string already_existing_user_id;
-    std::string already_existing_user_login;
-    std::string already_existing_user_password;
-};
