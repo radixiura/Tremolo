ed38f4066dfb8064e58ee59a0549185f53a3a973
Radix_iura <radix_iura@protonmail.com>
2021-10-23 11:18:42 +0300

    Update functions.py

--- python_module/modules/functions.py
+++ python_module/modules/functions.py
@@ -1,2 +1,12 @@
 def greetings():
     print("Привет!")
+
+
+def get_login():
+    user_login = input("Введите ваш логин: ")
+    return user_login
+
+
+def get_password():
+    user_password = input("Введите ваш пароль: ")
+    return user_password
