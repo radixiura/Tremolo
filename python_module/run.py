ceab4dc07608227fb3c2c1f06eb3952cb17038b4
Radix_iura <radix_iura@protonmail.com>
2021-10-23 11:18:14 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -15,6 +15,7 @@ checking = True
 while checking:
     if user_answer == "0":
         new_user_registration.new_user_registration()
+        user_authentication.user_authentication()
         main_menu.main_menu_buttons()
     elif user_answer == "1":
         user_authentication.user_authentication()
