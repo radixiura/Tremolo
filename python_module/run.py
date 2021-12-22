1be6b5b41895bfe391440994369b48eaf1f0a1b2
Radix_iura <radix_iura@protonmail.com>
2021-12-23 02:11:19 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 # Connecting a file with functions
-from functions import greetings, new_user_registration, user_authentication
-from main_menu_functions import main_menu
+import subprocess
+import sys
+from functions import new_user_registration, user_authentication
+from main_menu_modules import main_menu
 
 # from modules import postgresql_connection
 # postgresql_connection.connect_postgresql()
+
+result = subprocess.run([r"C:\Users\Radix\Desktop\Tremolo\python_module\modules", "-c"])
 
 print("Здравствуйте!")
 user_answer = input("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ")
