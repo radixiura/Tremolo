6b02d6fd02c759429a9b5b102bca9769d463af71
Radix_iura <radix_iura@protonmail.com>
2021-12-23 02:29:18 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # Connecting a file with functions
+import os
 import subprocess
 import sys
+from sys import platform
 from functions import new_user_registration, user_authentication
 from main_menu_modules import main_menu
 
 # from modules import postgresql_connection
 # postgresql_connection.connect_postgresql()
 
-result = subprocess.run([r"C:\Users\Radix\Desktop\Tremolo\python_module\modules", "-c"])
+if platform == "linux" or platform == "linux2":
+    print('ln')
+elif platform == "darwin":
+    print('qdwd')
+elif platform == "win32":
+    print('wdd')
+
+os.system(r"modules\dist\design.exe")
 
 print("Здравствуйте!")
 user_answer = input("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ")
