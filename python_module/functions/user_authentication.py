9e143b1e2723f7c5451e710d5dfcc6915f503c14
Radix_iura <radix_iura@protonmail.com>
2022-01-22 15:34:58 +0300

    Update user_authentication.py

--- python_module/functions/user_authentication.py
+++ python_module/functions/user_authentication.py
@@ -1,17 +1,13 @@
-from functions.sub_functions import s_functions
-
-
 def user_authentication():
-    login = s_functions.get_login()
+    login = input("Введите ваш логин: ")
     # if old_user_login exist in DB:
     #   pass
     # else:
     #   print('Данный логин не найден в базе данных. Попробуете ввести логин еще раз?: ')
-    user_password = input("Введите ваш пароль: ")
-    if len(user_password) > 8:
-        password = s_functions.get_password()
-        print(f"{password}, вы успешно вошли.")
+    password = input("Введите ваш пароль: ")
+    print(f"{login}, вы успешно вошли.")
     # if old_user_password exist in DB:
     #   pass
     # else:
     #   print('Пароль введен неверно. Попробуете ввести пароль еще раз?: ')
+    return login, password
