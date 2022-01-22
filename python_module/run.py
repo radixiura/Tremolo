734c3f9c9f37a3e4efa0d5b0460aace4167ce2db
Radix_iura <radix_iura@protonmail.com>
2022-01-22 14:58:26 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -5,22 +5,17 @@ import os
 import subprocess
 import sys
 from sys import platform
-from functions import new_user_registration, user_authentication
+from functions import greetings, new_user_registration, user_authentication
 from main_menu_modules import main_menu
 
-# from modules import postgresql_connection
-# postgresql_connection.connect_postgresql()
+user_answer = greetings.greetings()
 
-print("Здравствуйте!")
-user_answer = input("Вы уже имеете аккаунт? Введите 1 если да, 0 если нет: ")
-checking = True
-while checking:
-    if user_answer == "0":
-        new_user_registration.new_user_registration()
-        user_authentication.user_authentication()
-        main_menu.main_menu_buttons()
-    elif user_answer == "1":
-        user_authentication.user_authentication()
-        main_menu.main_menu_buttons()
-    else:
-        print("Ошибка. Вы ввели что - то странное.")
+if user_answer == "0":
+    new_user_registration.new_user_registration()
+    user_authentication.user_authentication()
+    main_menu.main_menu_buttons()
+elif user_answer == "1":
+    user_authentication.user_authentication()
+    main_menu.main_menu_buttons()
+else:
+    print("Ошибка. Вы ввели что - то странное.")
