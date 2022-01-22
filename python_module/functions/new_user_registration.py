6afd1ba385e755af3a45ae83e4ae9e85cf764b92
Radix_iura <radix_iura@protonmail.com>
2022-01-22 15:13:35 +0300

    Update new_user_registration.py

--- python_module/functions/new_user_registration.py
+++ python_module/functions/new_user_registration.py
@@ -1,15 +1,17 @@
 def new_user_registration():
+    # Логин нового пользователя
     new_user_login = input("Самое время зарегистрироваться. Введите свой новый логин:  ")
+    # Проверка достаточности длины логина
     while len(new_user_login) < 8:
         print('Логин должен содержать более 8ми символов. Попробуйте заново.')
         new_user_login = input()
         if len(new_user_login) > 8:
             break
         print(f"Ваш новый логин: {new_user_login}\n")
-
-        print("Самое время придумать пароль. Он должен содержать более 8ми символов: ")
-        new_user_password = input("Введите ваш новый пароль: ")
-        while len(new_user_password) < 8:
+    # Пароль нового пользователя
+    new_user_password = input("Введите ваш новый пароль: ")
+    # Проверка достаточности длины пароля
+    while len(new_user_password) < 8:
             print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
             new_user_password = input()
             if len(new_user_password) > 8:
