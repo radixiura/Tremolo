beba303b2df87b60376f994b2c9de66ec56d9d72
Radix_iura <radix_iura@protonmail.com>
2022-01-22 15:20:53 +0300

    Update new_user_registration.py

--- python_module/functions/new_user_registration.py
+++ python_module/functions/new_user_registration.py
@@ -7,19 +7,20 @@ def new_user_registration():
         new_user_login = input()
         if len(new_user_login) > 8:
             break
-        print(f"Ваш новый логин: {new_user_login}\n")
+
     # Пароль нового пользователя
     new_user_password = input("Введите ваш новый пароль: ")
     # Проверка достаточности длины пароля
     while len(new_user_password) < 8:
-            print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
-            new_user_password = input()
-            if len(new_user_password) > 8:
-                break
-        new_user_password_confirmation = input("Введите свой новый пароль еще раз: ")
-        while new_user_password != new_user_password_confirmation:
-            print('Введенные пароли не сходятся. Попробуйте еще раз.\n')
-            new_user_password_confirmation = input()
-            if new_user_password_confirmation == new_user_password:
-                break
-        return new_user_password_confirmation
+        print('Пароль должен содержать более 8ми символов. Попробуйте заново.')
+        new_user_password = input()
+        if len(new_user_password) > 8:
+            break
+            
+    # Подтверждение нового пароля
+    new_user_password_confirmation = input("Введите свой новый пароль еще раз: ")
+    while new_user_password != new_user_password_confirmation:
+        print('Введенные пароли не сходятся. Попробуйте еще раз.\n')
+        new_user_password_confirmation = input()
+        if new_user_password_confirmation == new_user_password:
+            break
