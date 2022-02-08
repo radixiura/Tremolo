d21bcceec0f487ac969e7a07801aa64965e764fc
Radix_iura <radix_iura@protonmail.com>
2022-02-09 00:28:10 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -37,67 +37,6 @@ def execute_query(connection, query):
         print(f"The error '{e}' occurred")
 
 
-# Запрос на извлечение данных из БД
-# Параметры функции: (Скрипт подключения к бд, будущий запрос)
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-
-select_users = "SELECT * from users"
-users = execute_read_query(connection, select_users)
-
-for user in users:
-    print(user)
-
-
-# Запрос на обновление информации в БД
-select_message_destination = "SELECT destination FROM messages WHERE id = 2"
-message_destination = execute_read_query(connection, select_message_destination)
-
-for destination in message_destination:
-    print(destination)
-
-update_message_destination = """
-UPDATE
-  messages
-SET
-  destination = "The weather has become pleasant now"
-WHERE
-  id = 2
-"""
-
-execute_query(connection, update_message_destination)
-
-
-# Шаблон обновления записи в БД
-select_message_destination = "SELECT destination FROM messages WHERE id = 2"
-message_destination = execute_read_query(connection, select_message_destination)
-
-for destination in message_destination:
-    print(destination)
-
-update_message_destination = """
-UPDATE
-  messages
-SET
-  destination = "The weather has become pleasant now"
-WHERE
-  id = 2
-"""
-execute_query(connection, update_message_destination)
-
-# Шаблон удаления записи из БД
-delete_comment = "DELETE FROM messages WHERE id = 5"
-execute_query(connection, delete_comment)
-
-
 # Основная часть
 # В переменную user_answer помещаем возвращаемое значение функции приветствия (0/1)
 user_answer = greeting.greeting_function()
