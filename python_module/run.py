da4a49c7a923bb1aee151560f28f4d9616cfe2fc
Radix_iura <radix_iura@protonmail.com>
2022-02-08 23:08:57 +0300

    Update run.py

--- python_module/run.py
+++ python_module/run.py
@@ -76,64 +76,6 @@ WHERE
 execute_query(connection, update_message_destination)
 
 
-# Создание таблицы пользователей со столбцами (id, логин, пароль, ранг)
-create_users_table = """
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  login_name TEXT NOT NULL,
-  password TEXT NOT NULL,
-  rank TEXT NOT NULL
-);
-"""
-
-# Заполнение данных в таблице пользователей
-create_users = """
-INSERT INTO
-  users (login_name, password, rank)
-VALUES
-  ('bigdaddy', 'porridge1', 'principale'),
-  ('joplej', 'gavnostar0', 'slave');
-"""
-
-
-# Создание таблицы сообщений со столбцами (id, сообщение, адресат, id адресанта)
-create_messages_table = """
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  msg TEXT NOT NULL, 
-  destination TEXT NOT NULL,
-  user_id INTEGER NOT NULL,  
-  FOREIGN KEY (user_id) REFERENCES users (id)
-);
-"""
-
-# Заполнение данных в таблице сообщений
-create_messages = """
-INSERT INTO
-  messages (msg, destination, user_id)
-VALUES
-  ("Happy", "I am feeling very happy today", 1),
-  ("Hot Weather", "The weather is very hot today", 2),
-  ("Help", "I need some help with my work", 2),
-  ("Great News", "I am getting married", 1),
-  ("Interesting Game", "It was a fantastic game of tennis", 5),
-  ("Party", "Anyone up for a late-night party today?", 3);
-"""
-
-# Запросы [Создание таблицы пользователей]
-
-# Параметры функции: (Скрипт подключения к бд, создание таблицы пользователей)
-execute_query(connection, create_users_table)
-
-# Параметры функции: (Скрипт подключения к бд, создание таблицы сообщений)
-execute_query(connection, create_messages_table)
-
-# Параметры функции: (Скрипт подключения к бд, заполнение таблицы пользователей)
-execute_query(connection, create_users)
-
-# Параметры функции: (Скрипт подключения к бд, заполнение таблицы сообщений)
-execute_query(connection, create_messages)
-
 # Шаблон обновления записи в БД
 select_message_destination = "SELECT destination FROM messages WHERE id = 2"
 message_destination = execute_read_query(connection, select_message_destination)
