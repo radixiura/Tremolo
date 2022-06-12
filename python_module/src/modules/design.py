34014aa3dc84a7c150823d0a9b0cce816413d42c
radixiura <radix_iura@protonmail.com>
2022-06-12 16:21:06 +0300

    Commit

--- python_module/src/modules/design.py
+++ python_module/src/modules/design.py
@@ -2,8 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
-    QTextEdit, QGridLayout, QApplication)
+
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+try:
+    # Включите в блок try/except, если вы также нацелены на Mac/Linux
+    from PyQt5.QtWinExtras import QtWin                                         #  !!!
+    myappid = 'mycompany.myproduct.subproduct.version'                          #  !!!
+    QtWin.setCurrentProcessExplicitAppUserModelID(myappid)                      #  !!!
+except ImportError:
+    pass
 
 
 class Example(QWidget):
@@ -13,11 +24,25 @@ class Example(QWidget):
 
         self.initUI()
 
+        app = QtWidgets.QApplication(sys.argv)
+        app.setWindowIcon(QtGui.QIcon('icon.png'))
+
+        window = QtWidgets.QWidget()
+        window.setWindowIcon(QtGui.QIcon('icon.png'))
+        window.show()
+
     def initUI(self):
 
         receiver = QLabel('Получатель')
         author = QLabel('Тема')
         review = QLabel('Сообщение')
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        quit_btn = QPushButton('Выйти', self)
+        quit_btn.clicked.connect(QApplication.instance().quit)
 
         receiver_edit = QLineEdit()
         theme_edit = QLineEdit()
@@ -37,8 +62,8 @@ class Example(QWidget):
 
         self.setLayout(grid)
 
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Review')
+        self.setGeometry(450, 450, 500, 450)
+        self.setWindowTitle('Tremolo - Отправить сообщение')
         self.show()
 
 
